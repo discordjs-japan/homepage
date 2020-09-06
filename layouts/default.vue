cc386380115034a05f6b14519db2c69c88065caa
InkoHX <inkohx@gmail.com>
2020-09-07 08:39:40 +0900

    fix: layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
   <v-app>
-    <v-content>
+    <v-main>
       <nuxt />
-    </v-content>
+    </v-main>
 
     <Footer />
 
@@ -12,7 +12,6 @@
 
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
-import { MetaInfo } from 'vue-meta'
 
 const Fab = () => import('@/components/layout/home/Fab.vue')
 const Footer = () => import('@/components/layout/home/Footer.vue')
@@ -23,13 +22,5 @@ const Footer = () => import('@/components/layout/home/Footer.vue')
     Footer
   }
 })
-export default class extends Vue {
-  public head (): MetaInfo {
-    return {
-      link: [
-        { rel: 'canonical', href: `https://discordjs-japan.org${this.$route.path}` }
-      ]
-    }
-  }
-}
+export default class extends Vue {}
 </script>
