04638cd176597f65897d888876245142c2debf15
InkoHX <me@inkohx.dev>
2023-04-03 17:13:19 +0900

    disable SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
-import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  output: 'server',
-  adapter: cloudflare(),
 })
